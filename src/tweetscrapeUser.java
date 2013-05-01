b8eab46d54d2d73dec3be52c567337149ea7ea05
Beau Bouchard <beau@beaubouchard.com>
2013-05-01 16:23:20 -0300

    Create tweetscrapeUser.java

---
+++ src/tweetscrapeUser.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Beau Bouchard
+ * Date: 4/4/2013
+ * Last Updated: 4/10/2013
+ * Description: 
+ * 
+ * 
+ *  
+ **/
+import java.sql.*;
+
+public class tweetscrapeUser {
+
+  //User -- from obj class User
+	private long userID;
+   private String screenName;
+   private String userDescription;
+
+}
