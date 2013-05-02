e331c7d603dc113241ee59f7def2c5daf8ad8c47
Beau Bouchard <beau@beaubouchard.com>
2013-05-02 11:23:59 -0300

    Create tweetscrapeScrape.java

---
+++ src/tweetscrapeScrape.java
@@ -0,0 +1,18 @@
+/**
+ * Author: Beau Bouchard
+ * Date: 2013/04/04
+ * Last Updated: 2013/05/02
+ * Description: Each tweet entity will be linked to a scrape
+ * 
+ * 
+ *  
+ **/
+import java.sql.*;
+
+public class tweetscrapeScrape {
+  private int scrapeID;
+	private Date start_at; 
+	private Date stop_at; 
+	private String searchREGEX;
+
+}
