52d731e0452982ef0b5545790db4a0e1ddefdc6e
Beau Bouchard <beau@beaubouchard.com>
2014-01-23 15:23:43 -0500

    updated description to tweetscrapeUser.java
    Working out some of the data base logic still. I know there is a unique ID which twitter assigns to users, as the bio/username can be changed. Need to store that

--- src/tweetscrapeUser.java
+++ src/tweetscrapeUser.java
@@ -1,19 +1,29 @@
 /**
  * Author: Beau Bouchard
- * Date: 4/4/2013
- * Last Updated: 4/10/2013
+ * Date: 2013/04/04
+ * Last Updated: 2014/01/23
  * Description: 
  * 
- * 
+ *       ts_tweets, the main tweet object will have a many to one relationship with this (ts_twitteruser) many tweets which are all mapped to the same username. 
+ *    It should be noted that there is a userDescription or the twitter user's bio, this is recorded once when the detector first times the first tweet from 
+ *    a user not yet in the database thus creating a new entry. If the Bio changes, the changes will not be recorded at this time.  
  *  
  **/
 import java.sql.*;
 
 public class tweetscrapeUser {
 
-  //User -- from obj class User
+	//User -- from obj class User
 	private long userID;
+   //private int uniqueTwitterID;
    private String screenName;
    private String userDescription;
+   
+   //ts_twitteruser
+   //twitter_id
+   //twitter_username
+   //twitter_bio
+   
+   
 
 }
