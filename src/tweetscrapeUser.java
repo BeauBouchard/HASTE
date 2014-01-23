3c1198c54aec8dea723a09b3a0cc29153efddb30
Beau Bouchard <beau@beaubouchard.com>
2014-01-23 15:35:47 -0500

    added init and mutator functions to user

--- src/tweetscrapeUser.java
+++ src/tweetscrapeUser.java
@@ -14,7 +14,7 @@ import java.sql.*;
 public class tweetscrapeUser {
 
 	//User -- from obj class User
-	private long userID;
+	private int userID;
    //private int uniqueTwitterID;
    private String screenName;
    private String userDescription;
@@ -22,8 +22,84 @@ public class tweetscrapeUser {
    //ts_twitteruser
    //twitter_id
    //twitter_username
+   //uniqueTwitterID
    //twitter_bio
+   	
+	public tweetscrapeUser()
+	{
+		
+	
+		
+	}
+	
+	public tweetscrapeUser(int inc_userID, String inc_screenName)
+	{
+		
+	
+		
+	}
+   
+   	
+	/**
+	 * setuserID
+	 * Description - used for setting the userID of object
+	 * @param inc_userID - the ID of the user obj inside local database
+	 */
+	public void setuserID(int inc_userID)
+	{
+		userID = inc_userID;
+	}
+   
+   /**
+	 * setscreenName
+	 * Description - used for setting the screenName of object
+	 * @param inc_screenName - Incomming String of the twitter user's username
+	 */
+	public void setscreenName(String inc_screenName)
+	{
+		screenName = inc_screenName;
+	}
+   /**
+    * setuserDescription
+    * Description - used for setting the userDescription of object, userDescription will be the twitter bio of user's profile at first tweet
+    * @param inc_userDescription - Incomming string of the twitter user's bio
+    */
+   public void setuserDescription(String inc_userDescription)
+   {
+      userDescription = inc_userDescription;
+   }
    
    
-
+   
+   
+   
+   /**
+	 * getuserID
+	 * Description - used for getting  the userID of object
+	 * @return userID - the ID of the user obj inside local database
+	 */
+   public int getuserID()
+   {
+      return userID;
+   }
+   
+   /**
+	 * getscreenName
+	 * Description - used for setting the screenName of object
+	 * @return screenName - String of the twitter user's username
+	 */
+   public String getscreenName()
+   {
+      return screenName;
+   }
+   
+   /**
+    * getuserDescription
+    * Description - userDescription will be the twitter bio of user's profile at first tweet
+    * @return userDescription - string of the twitter user's bio
+    */
+   public String getuserDescription()
+   {
+      return userDescription;
+   }
 }
